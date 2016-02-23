8bf88ea2c70a641ea1b4878859a43908402cb368
SystemRezerwacjiHotelowej <rezerwacjahotelowa2016@interia.pl>
2016-02-23 21:21:52 +0100

    Utworzenie Projektu na gitHub

---
+++ SystemRezerwacji/SystemRezerwacji/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SystemRezerwacji.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SystemRezerwacji/SystemRezerwacji/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SystemRezerwacji.Startup))]
+namespace SystemRezerwacji
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
